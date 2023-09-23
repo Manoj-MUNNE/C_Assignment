bc5dfc607aae885777342f2a254ba4c5c570c96b
Manoj-MUNNE <manojmunne28@gmail.com>
2023-09-23 19:26:05 +0530

    my first assignment on git

---
+++ Assignment_1/0to100.c
@@ -0,0 +1,11 @@
+//Numbers from 1 to 100 in different lines
+
+#include<stdio.h>
+int main()
+{
+for(int i=1; i<=100; i++)
+{
+printf("\n %d \n", i);
+}
+return 0;
+}
---
+++ Assignment_1/formanoj.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+for(int i=1; i<=10; i=i+2)
+{
+printf("Hello Manoj\n");
+}
+return 0;
+}
